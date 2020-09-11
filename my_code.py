49b7d1cc3694d38e93600e94cd364f96c9c7fc6f
ChienT1993 <21ctran@athenian.org>
2020-09-11 17:33:01 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -1,3 +1,16 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  
 
+name= input("Enter your name: ")
+print ("???: Hello there, " + name + " , I'm Emma, your automatic-personal-survivor device, welcome to Mars!")
+print ("Emma: You are the only human here and there is no edible plant or animal, how about we plant some vegtable?")
+plant= input ("Emma: What kind of vegtable do you want to plant? ")
+print ("Emma: " + plant + " huh?, good choice, now we need some fresh water.") 
+direction= input ("Emma: Which direction do you want to take? ")
+print ("Emma: Alright, " + direction + " it is")
+print ("*20 minutes later")
+print ("Emma: Oh look, an alien, let's go talk to him")
+greet= input("What do you want to say? ")
+print (name + ": " + greet)
+print ("Alien: fieuhgiudr,jgklsecseiughrai,uhfcesuie!!!")
+print ("Emma: It's look like he is angry, run!!!!!")
